1cf35029746fbc96cb54a7deaf6ab2b545b9f234
sanu4ka <sanu4ka1989@gmail.com>
2023-02-15 15:12:36 +0200

    form fix

--- src/page/Login/Login.jsx
+++ src/page/Login/Login.jsx
@@ -11,6 +11,7 @@ const Login = () => {
         py: 6,
         justifyContent: 'center',
         alignContent: 'center',
+        flexDirection: 'column',
       }}
     >
       <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
@@ -23,6 +24,10 @@ const Login = () => {
             alignItems: 'center',
           }}
         >
+          <Typography variant="h7" component="h7" marginBottom="15px">
+            Welcome to my SimplePhonebook. To use all the functionality,
+            register or log in.
+          </Typography>
           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
           <Typography component="h1" variant="h5">
             Sign in
--- src/page/Register/Register.jsx
+++ src/page/Register/Register.jsx
@@ -1,30 +1,41 @@
 import { RegisterForm } from '../../components/RegisterForm/RegisterForm';
-import { Avatar, Box, Container, Typography } from '@mui/material';
+import { Grid, Avatar, Box, Paper, Typography } from '@mui/material';
 
 const Register = () => {
   return (
-    <Container
+    <Grid
+      container
       component="main"
-      maxWidth="xs"
       sx={{
+        height: '100vh',
         py: 6,
+        justifyContent: 'center',
+        alignContent: 'center',
+        flexDirection: 'column',
       }}
     >
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
+    <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <Box
+          sx={{
+            my: 8,
+            mx: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+        <Typography variant="h7" component="h7" marginBottom="15px">
+          Welcome to my SimplePhonebook. To use all the functionality, register
+          or log in.
+        </Typography>
         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
         <RegisterForm />
       </Box>
-    </Container>
+      </Grid>
+    </Grid>
   );
 };
 
